715f9ef31e2c44628d7c6ea8c0d545536f3b857b
Pavlo Daniv <paulsegfault@gmail.com>
2023-04-09 00:56:10 +0300

    Fix ecr resource names

--- main.tf
+++ main.tf
@@ -93,13 +93,13 @@ resource "aws_codeartifact_repository_permissions_policy" "promotion" {
   repository      = aws_codeartifact_repository.promotion.repository
 }
 
-resource "aws_ecr_repository" "taxi-trip-client" {
+resource "aws_ecr_repository" "taxi_trip_client" {
   name                 = "taxi-trip-client"
   image_tag_mutability = "MUTABLE"
   force_delete        = true
 }
 
-resource "aws_ecr_repository" "taxi-trip-api" {
+resource "aws_ecr_repository" "taxi_trip_api" {
   name                 = "taxi-trip-api"
   image_tag_mutability = "MUTABLE"
   force_delete        = true
