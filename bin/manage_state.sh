d1c1b2186ead512f1e34e074bc7b8f1f844dd8f2
Pavlo Daniv <paulsegfault@gmail.com>
2023-05-04 22:35:16 +0300

    Remove scratch mode

--- bin/manage_state.sh
+++ bin/manage_state.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 ACTION=$1
-SCRATCH=$2
 AVAILABLE_ACTIONS="Available actions: [apply, destroy]"
 
 cd "$(cd "$(dirname "$0")/.."; pwd)"
@@ -13,23 +12,12 @@ fi
 
 if [[ "$ACTION" = "apply" ]]; then
   echo "Creating infrastructure"
-  if [[ "$SCRATCH" = "scratch" ]]; then
-    echo "Creating setup from scratch!"
-    echo "Creating additional unmanaged resources for terraform..."
-    echo "Creating dynamoDB table for locking..."
-    aws dynamodb create-table --cli-input-json file://state_table.json --region us-east-2
-  fi
   terraform init
   terraform apply --auto-approve
 elif [[ "$ACTION" = "destroy" ]]; then
   echo "Destroying infrastructure"
   terraform init
   terraform destroy --auto-approve
-  if [[ "$SCRATCH" ]]; then
-    echo "Deleting all related resources to this setup!"
-    echo "Destroying additional unmanaged resources"
-    aws dynamodb delete-table --table-name yellow-taxi-tf-state --region us-east-2
-  fi
 else
   echo "Unknown action provided: $ACTION. Available actions: $AVAILABLE_ACTIONS"
 fi
