d3225af92881ec7feffefc768b876ca692cab68c
Pavlo <paulsegfault@gmail.com>
2023-05-18 23:03:39 +0300

    Add additional ECR repositories

--- 9-ecr.tf
+++ 9-ecr.tf
@@ -9,3 +9,15 @@ resource "aws_ecr_repository" "taxi_trip_api" {
   image_tag_mutability = "MUTABLE"
   force_delete        = true
 }
+
+resource "aws_ecr_repository" "taxi_trip_facade" {
+  name                 = "taxi-trip-facade"
+  image_tag_mutability = "MUTABLE"
+  force_delete        = true
+}
+
+resource "aws_ecr_repository" "taxi_trip_totals" {
+  name                 = "taxi-trip-totals"
+  image_tag_mutability = "MUTABLE"
+  force_delete        = true
+}
