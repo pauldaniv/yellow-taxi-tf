6089f7d5769e61a4fe8cf5e5243b2d82b3d5081e
Pavlo <paulsegfault@gmail.com>
2023-05-24 21:38:45 +0300

    Fix service account policy

--- 3-iam.tf
+++ 3-iam.tf
@@ -103,7 +103,7 @@ data "aws_iam_policy_document" "assume-service-account-secrets-policy" {
       test     = "StringEquals"
       variable = "${module.eks.oidc_provider}:sub"
       values   = [
-        "system:serviceaccount:*:secret-accessor"
+        "system:serviceaccount:*:*-service-account"
       ]
     }
     condition {
