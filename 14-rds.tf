1ce930a3290d279219640e9448c36c86e18ebada
Pavlo <paulsegfault@gmail.com>
2023-05-31 23:16:57 +0300

    Update RDS security groups

--- 14-rds.tf
+++ 14-rds.tf
@@ -1,6 +1,18 @@
 resource "aws_db_subnet_group" "public" {
   name       = "public"
   subnet_ids = module.vpc.public_subnets
+  tags = {
+    Name = "yellow-taxi"
+  }
+}
+
+resource "aws_db_subnet_group" "private" {
+  name       = "private-subnet"
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Name = "yellow-taxi"
+  }
 }
 
 
@@ -10,6 +22,8 @@ data "aws_secretsmanager_secret_version" "db" {
 
 resource "aws_db_instance" "postgres" {
   db_name           = "service"
+  username = "service"
+  password = data.aws_secretsmanager_secret_version.db.secret_string
   engine            = "postgres"
   engine_version    = "15"
   instance_class    = "db.t4g.micro"
@@ -17,10 +31,8 @@ resource "aws_db_instance" "postgres" {
 
   publicly_accessible  = true
   skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.public.name
-
-  username = "service"
-  password = data.aws_secretsmanager_secret_version.db.secret_string
+  db_subnet_group_name = aws_db_subnet_group.private.name
+  vpc_security_group_ids = [aws_default_security_group.vpc_security_group.id]
 }
 
 output "db_instance_url" {
