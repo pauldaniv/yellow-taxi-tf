c74c82780bf2c8d36dfb99c8eb9f18ce74439610
Pavlo <paulsegfault@gmail.com>
2023-06-01 19:58:01 +0300

    Apply

--- 14-rds.tf
+++ 14-rds.tf
@@ -1,20 +1,14 @@
-resource "aws_db_subnet_group" "public" {
-  name       = "public"
-  subnet_ids = module.vpc.public_subnets
-  tags = {
-    Name = "yellow-taxi"
-  }
-}
-
-resource "aws_db_subnet_group" "private" {
+resource "aws_db_subnet_group" "db" {
   name       = "private-subnet"
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = concat(
+    module.vpc.private_subnets,
+    module.vpc.public_subnets
+  )
 
   tags = {
     Name = "yellow-taxi"
   }
 }
-
 
 data "aws_secretsmanager_secret_version" "db" {
   secret_id = "prod_yt_db_pass"
@@ -31,8 +25,8 @@ resource "aws_db_instance" "postgres" {
 
   publicly_accessible  = true
   skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.private.name
-  vpc_security_group_ids = [aws_default_security_group.vpc_security_group.id]
+  db_subnet_group_name = aws_db_subnet_group.db.name
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
 }
 
 output "db_instance_url" {
