c2fe10610e03ee34f877c6a21b981fbf684837f0
Pavlo <paulsegfault@gmail.com>
2023-06-04 00:49:12 +0300

    DB public access

--- 1-vpc.tf
+++ 1-vpc.tf
@@ -28,7 +28,8 @@ module "vpc" {
 
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
-  create_database_nat_gateway_route      = true
+#  create_database_nat_gateway_route      = true
+  create_database_internet_gateway_route = true
   enable_dns_hostnames                   = true
   enable_dns_support                     = true
   tags                                   = {
