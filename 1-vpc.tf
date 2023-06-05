f54fd63d4941170c1accc3094056e4a5bdcc1225
Pavlo <paulsegfault@gmail.com>
2023-06-05 14:00:01 +0300

    Use single nat and db nat route table

--- 1-vpc.tf
+++ 1-vpc.tf
@@ -17,7 +17,7 @@ module "vpc" {
   }
 
   enable_nat_gateway     = true
-  single_nat_gateway     = false
+  single_nat_gateway     = true
   one_nat_gateway_per_az = false
   database_subnets       = [
     "10.0.140.0/24",
@@ -29,7 +29,7 @@ module "vpc" {
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
   create_database_nat_gateway_route      = true
-  create_database_internet_gateway_route = true
+#  create_database_internet_gateway_route = true
   enable_dns_hostnames                   = true
   enable_dns_support                     = true
   tags                                   = {
