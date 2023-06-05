2a71ce949ff8beeedc429d11ea207a3320445143
Pavlo <paulsegfault@gmail.com>
2023-06-05 16:33:26 +0300

    Fix route_table_association

--- 14-rds.tf
+++ 14-rds.tf
@@ -93,7 +93,7 @@ resource "aws_db_instance" "postgres" {
 
   publicly_accessible    = true
   skip_final_snapshot    = true
-  db_subnet_group_name   = aws_db_subnet_group.database
+  db_subnet_group_name   = aws_db_subnet_group.database.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
 }
 
