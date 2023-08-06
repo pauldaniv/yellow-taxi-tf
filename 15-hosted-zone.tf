896a706270cb6416924838db08f50d9bf688d68d
Pavlo <paulsegfault@gmail.com>
2023-08-06 23:26:53 +0300

    Add cert-manager assume role, policy and policy attachment

--- 15-hosted-zone.tf
+++ 15-hosted-zone.tf
@@ -52,14 +52,55 @@ data "aws_iam_policy_document" "cert_manager_route53_policy" {
       "route53:ChangeResourceRecordSets",
       "route53:ListResourceRecordSets"
     ]
-    resources = ["arn:aws:route53:::hostedzone/${module.zones.route53_zone_zone_id[keys(module.zones.route53_zone_zone_id)[0]]}"]
+    resources = [
+      "arn:aws:route53:::hostedzone/${module.zones.route53_zone_zone_id[keys(module.zones.route53_zone_zone_id)[0]]}"
+    ]
   }
 }
 
-resource "aws_iam_role" "cert_manager_acme" {
+data "aws_iam_policy_document" "cert_manager_route53_assume_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
+    }
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "${module.eks.oidc_provider}:sub"
+      values   = [
+        "system:serviceaccount:*:api-service-account",
+        "system:serviceaccount:*:totals-service-account",
+        "system:serviceaccount:*:*-service-account"
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${module.eks.oidc_provider}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "cert_manager_acme_role" {
   name               = "eks-cert-manager-acme"
-  assume_role_policy = data.aws_iam_policy_document.cert_manager_route53_policy.json
+  assume_role_policy = data.aws_iam_policy_document.cert_manager_route53_assume_policy.json
   tags               = {
     Project = "yellow-taxi"
   }
+}
+
+resource "aws_iam_policy" "cert_manager_policy" {
+  name        = "secrets-manager-policy"
+  description = "Allows access to Secrets Manager"
+  policy = data.aws_iam_policy_document.cert_manager_route53_policy.json
+}
+
+resource "aws_iam_policy_attachment" "cert_manager_role_policy_attachment" {
+  name       = "eks_service_account_role_attachment"
+  roles      = [aws_iam_role.cert_manager_acme_role.name]
+  policy_arn = aws_iam_policy.cert_manager_policy.arn
 }
