8071b34f3a8f7fc689806037fc9e3947001c180a
Pavlo <paulsegfault@gmail.com>
2023-08-26 20:20:27 +0300

    Add RDS access to nodes: action: apply

--- 14-rds.tf
+++ 14-rds.tf
@@ -1,114 +1,123 @@
-#resource "aws_subnet" "db_subnet" {
-#  count = var.db_public_access ? length(var.public_db_subnets) : 0
+##resource "aws_subnet" "db_subnet" {
+##  count = var.db_public_access ? length(var.public_db_subnets) : 0
+##
+##  vpc_id               = module.vpc.vpc_id
+##  cidr_block           = var.public_db_subnets[count.index]
+##  availability_zone    = length(regexall("^[a-z]{2}-", element(module.vpc.azs, count.index))) > 0 ? element(module.vpc.azs, count.index) : null
+##  availability_zone_id = length(regexall("^[a-z]{2}-", element(module.vpc.azs, count.index))) == 0 ? element(module.vpc.azs, count.index) : null
+##
+##
+##  tags = {
+##    "Name" = format(
+##      "yellow-taxi-db-%s",
+##      element(module.vpc.azs, count.index),
+##    )
+##  }
+##}
 #
-#  vpc_id               = module.vpc.vpc_id
-#  cidr_block           = var.public_db_subnets[count.index]
-#  availability_zone    = length(regexall("^[a-z]{2}-", element(module.vpc.azs, count.index))) > 0 ? element(module.vpc.azs, count.index) : null
-#  availability_zone_id = length(regexall("^[a-z]{2}-", element(module.vpc.azs, count.index))) == 0 ? element(module.vpc.azs, count.index) : null
+##resource "aws_route_table" "db_route_table" {
+##  count  = var.db_public_access ? 1 : 0
+##  vpc_id = module.vpc.vpc_id
+##  tags   = {
+##    "Name" = "main-db-public"
+##  }
+##}
 #
+##resource "aws_route_table_association" "db_route_table_association" {
+##  count = var.db_public_access ? length(module.vpc.database_subnets) : 0
+##
+##  subnet_id      = element(aws_subnet.db_subnet[*].id, count.index)
+##  route_table_id = aws_route_table.db_route_table[0].id
+##}
+##
+##resource "aws_route" "database_internet_route" {
+##  count                  = var.db_public_access ? 1 : 0
+##  route_table_id         = aws_route_table.db_route_table[0].id
+##  destination_cidr_block = "0.0.0.0/0"
+##  gateway_id             = module.vpc.igw_id
+##
+##  timeouts {
+##    create = "5m"
+##  }
+##}
+#
+##resource "aws_route" "database_nat_gateway" {
+##  count = var.db_public_access == false ? 1 : 0
+##
+##  route_table_id         = element(aws_route_table.db_route_table[*].id, count.index)
+##  destination_cidr_block = "0.0.0.0/0"
+##  nat_gateway_id         = element(module.vpc.natgw_ids, count.index)
+##
+##  timeouts {
+##    create = "5m"
+##  }
+##}
+#
+##resource "aws_db_subnet_group" "db_subnet_group" {
+##  name        = "yt_db_sb_group"
+##  description = "Database subnet group for database"
+##  subnet_ids  = aws_subnet.db_subnet[*].id
+##
+##  tags = {
+##    "Name" : "yellow-taxi"
+##  }
+##}
+#
+#
+#resource "aws_security_group" "db_sg" {
+#  name        = "db_sg"
+#  description = "Postgres security group"
+#  vpc_id      = module.vpc.vpc_id
+#
+#  ingress {
+#    from_port   = 5432
+#    to_port     = 5432
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 #
 #  tags = {
-#    "Name" = format(
-#      "yellow-taxi-db-%s",
-#      element(module.vpc.azs, count.index),
-#    )
+#    Name = "yellow-taxi-db-sg"
 #  }
 #}
-
-#resource "aws_route_table" "db_route_table" {
-#  count  = var.db_public_access ? 1 : 0
-#  vpc_id = module.vpc.vpc_id
-#  tags   = {
-#    "Name" = "main-db-public"
-#  }
-#}
-
-#resource "aws_route_table_association" "db_route_table_association" {
-#  count = var.db_public_access ? length(module.vpc.database_subnets) : 0
 #
-#  subnet_id      = element(aws_subnet.db_subnet[*].id, count.index)
-#  route_table_id = aws_route_table.db_route_table[0].id
+#resource "aws_security_group_rule" "eks_node_group_outbound" {
+#  security_group_id = module.eks.node_security_group_id
+#  type              = "egress"
+#  from_port         = 5432
+#  to_port           = 5432
+#  protocol          = "tcp"
+#  source_security_group_id = aws_security_group.db_sg.id
 #}
 #
-#resource "aws_route" "database_internet_route" {
-#  count                  = var.db_public_access ? 1 : 0
-#  route_table_id         = aws_route_table.db_route_table[0].id
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = module.vpc.igw_id
-#
-#  timeouts {
-#    create = "5m"
-#  }
+#resource "aws_security_group_rule" "inbound_rule" {
+#  protocol                 = "tcp"
+#  type                     = "ingress"
+#  from_port                = 5432
+#  to_port                  = 5432
+#  security_group_id        = aws_security_group.db_sg.id
+#  source_security_group_id = module.eks.node_security_group_id
 #}
-
-#resource "aws_route" "database_nat_gateway" {
-#  count = var.db_public_access == false ? 1 : 0
 #
-#  route_table_id         = element(aws_route_table.db_route_table[*].id, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id         = element(module.vpc.natgw_ids, count.index)
-#
-#  timeouts {
-#    create = "5m"
-#  }
+#data "aws_secretsmanager_secret_version" "db" {
+#  secret_id = "prod_yt_db_pass"
 #}
-
-#resource "aws_db_subnet_group" "db_subnet_group" {
-#  name        = "yt_db_sb_group"
-#  description = "Database subnet group for database"
-#  subnet_ids  = aws_subnet.db_subnet[*].id
 #
-#  tags = {
-#    "Name" : "yellow-taxi"
-#  }
+#resource "aws_db_instance" "postgres" {
+#  db_name           = "service"
+#  username          = "service"
+#  password          = data.aws_secretsmanager_secret_version.db.secret_string
+#  engine            = "postgres"
+#  engine_version    = "15"
+#  instance_class    = "db.t4g.micro"
+#  allocated_storage = 10
+#
+#  publicly_accessible    = true
+#  skip_final_snapshot    = true
+#  db_subnet_group_name   = module.vpc.database_subnet_group_name
+#  vpc_security_group_ids = [aws_security_group.db_sg.id]
 #}
-
-
-resource "aws_security_group" "db_sg" {
-  name        = "db_sg"
-  description = "Postgres security group"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "yellow-taxi-db-sg"
-  }
-}
-
-resource "aws_security_group_rule" "inbound_rule" {
-  protocol                 = "tcp"
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  security_group_id        = aws_security_group.db_sg.id
-  source_security_group_id = module.eks.node_security_group_id
-}
-
-data "aws_secretsmanager_secret_version" "db" {
-  secret_id = "prod_yt_db_pass"
-}
-
-resource "aws_db_instance" "postgres" {
-  db_name           = "service"
-  username          = "service"
-  password          = data.aws_secretsmanager_secret_version.db.secret_string
-  engine            = "postgres"
-  engine_version    = "15"
-  instance_class    = "db.t4g.micro"
-  allocated_storage = 10
-
-  publicly_accessible    = true
-  skip_final_snapshot    = true
-  db_subnet_group_name   = module.vpc.database_subnet_group_name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-}
-
-output "db_instance_url" {
-  value = aws_db_instance.postgres.endpoint
-}
+#
+#output "db_instance_url" {
+#  value = aws_db_instance.postgres.endpoint
+#}
--- 15-hosted-zone.tf
+++ 15-hosted-zone.tf
@@ -1,109 +1,109 @@
-#module "zones" {
-#  source  = "terraform-aws-modules/route53/aws//modules/zones"
-#  version = "~> 2.0"
-#
-#  zones = {
-#    "yellow-taxi.me" = {
-#      tags = {
-#        env = "yellow-taxi"
-#      }
-#    }
-#  }
-#
-#  tags = {
-#    ManagedBy = "Terraform"
-#  }
-#}
-#
-#module "records" {
-#  source  = "terraform-aws-modules/route53/aws//modules/records"
-#  version = "~> 2.0"
-#
-#  zone_name = keys(module.zones.route53_zone_zone_id)[0]
-#
-#  records = [
-#    {
-#      name    = "iptest"
-#      type    = "A"
-#      ttl     = 300
-#      records = [
-#        "10.10.10.10",
-#        "10.10.10.11",
-#        "10.10.10.12",
-#      ]
-#    },
-#    {
-#      name    = "api"
-#      type    = "CNAME"
-#      ttl     = 300
-#      records = [
-#        "a9d7afddc30fa41d9b4e1aedde83abcc-fef38ce65b03bd80.elb.us-east-2.amazonaws.com",
-#      ]
-#    },
-#  ]
-#
-#  depends_on = [module.zones]
-#}
-#
-#
-#data "aws_iam_policy_document" "cert_manager_route53_policy" {
-#  statement {
-#    effect  = "Allow"
-#    actions = [
-#      "route53:GetChange",
-#      "secretsmanager:DescribeSecret"
-#    ]
-#    resources = ["arn:aws:route53:::change/*"]
-#  }
-#
-#  statement {
-#    effect  = "Allow"
-#    actions = [
-#      "route53:ChangeResourceRecordSets",
-#      "route53:ListResourceRecordSets"
-#    ]
-#    resources = [
-#      "arn:aws:route53:::hostedzone/${module.zones.route53_zone_zone_id[keys(module.zones.route53_zone_zone_id)[0]]}"
-#    ]
-#  }
-#}
-#
-#data "aws_iam_policy_document" "cert_manager_route53_assume_policy" {
-#  statement {
-#    effect  = "Allow"
-#    actions = [
-#      "sts:AssumeRoleWithWebIdentity"
-#    ]
-#    principals {
-#      type        = "Federated"
-#      identifiers = [module.eks.oidc_provider_arn]
-#    }
-#    condition {
-#      test     = "ForAnyValue:StringEquals"
-#      variable = "${module.eks.oidc_provider}:sub"
-#      values   = [
-#        "system:serviceaccount:cert-manager:cert-manager"
-#      ]
-#    }
-#  }
-#}
-#
-#resource "aws_iam_role" "cert_manager_acme_role" {
-#  name               = "eks-cert-manager-acme"
-#  assume_role_policy = data.aws_iam_policy_document.cert_manager_route53_assume_policy.json
-#  tags               = {
-#    Project = "yellow-taxi"
-#  }
-#}
-#
-#resource "aws_iam_policy" "cert_manager_policy" {
-#  name        = "cert_manager_policy"
-#  description = "Allows access to Route53"
-#  policy = data.aws_iam_policy_document.cert_manager_route53_policy.json
-#}
-#
-#resource "aws_iam_policy_attachment" "cert_manager_role_policy_attachment" {
-#  name       = "cert_manager_role_policy_attachment"
-#  roles      = [aws_iam_role.cert_manager_acme_role.name]
-#  policy_arn = aws_iam_policy.cert_manager_policy.arn
-#}
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    "yellow-taxi.me" = {
+      tags = {
+        env = "yellow-taxi"
+      }
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+
+  records = [
+    {
+      name    = "iptest"
+      type    = "A"
+      ttl     = 300
+      records = [
+        "10.10.10.10",
+        "10.10.10.11",
+        "10.10.10.12",
+      ]
+    },
+    {
+      name    = "api"
+      type    = "CNAME"
+      ttl     = 300
+      records = [
+        "a9d7afddc30fa41d9b4e1aedde83abcc-fef38ce65b03bd80.elb.us-east-2.amazonaws.com",
+      ]
+    },
+  ]
+
+  depends_on = [module.zones]
+}
+
+
+data "aws_iam_policy_document" "cert_manager_route53_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "route53:GetChange",
+      "secretsmanager:DescribeSecret"
+    ]
+    resources = ["arn:aws:route53:::change/*"]
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:ListResourceRecordSets"
+    ]
+    resources = [
+      "arn:aws:route53:::hostedzone/${module.zones.route53_zone_zone_id[keys(module.zones.route53_zone_zone_id)[0]]}"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "cert_manager_route53_assume_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
+    }
+    condition {
+      test     = "ForAnyValue:StringEquals"
+      variable = "${module.eks.oidc_provider}:sub"
+      values   = [
+        "system:serviceaccount:cert-manager:cert-manager"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "cert_manager_acme_role" {
+  name               = "eks-cert-manager-acme"
+  assume_role_policy = data.aws_iam_policy_document.cert_manager_route53_assume_policy.json
+  tags               = {
+    Project = "yellow-taxi"
+  }
+}
+
+resource "aws_iam_policy" "cert_manager_policy" {
+  name        = "cert_manager_policy"
+  description = "Allows access to Route53"
+  policy = data.aws_iam_policy_document.cert_manager_route53_policy.json
+}
+
+resource "aws_iam_policy_attachment" "cert_manager_role_policy_attachment" {
+  name       = "cert_manager_role_policy_attachment"
+  roles      = [aws_iam_role.cert_manager_acme_role.name]
+  policy_arn = aws_iam_policy.cert_manager_policy.arn
+}
